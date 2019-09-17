89e8213f06d6eb89e380a9d1ec7604fbfa171bd8
Johnson Kuang <jkuang7@cs.washington.edu>
2019-09-17 14:40:44 -0700

    added legend

--- new-visualization-code/viz-client/src/MapComponent.jsx
+++ new-visualization-code/viz-client/src/MapComponent.jsx
@@ -2,19 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom";
 import L from "leaflet";
 import ndjsonStream from "can-ndjson-stream";
-import squareGrid from '@turf/square-grid';
-import pointsWithinPolygon from '@turf/points-within-polygon'
+import squareGrid from "@turf/square-grid";
+import pointsWithinPolygon from "@turf/points-within-polygon";
+import "bootstrap/dist/css/bootstrap.css";
 
 import dcBounds from "./utils/dc-boundary";
-import orangeRedScale from './utils/colorScales';
-import querySidwalkData from "./utils/querySidewalkData";
+import colorScales from "./utils/colorScales";
 
-const LegendControl = function LegendControl(props) {    
+const LegendControl = function LegendControl(props) {
   //this will be called on every setstate
   //add renderGridLegend here
+  if (props.gridon) {
+    const jsx = (
+      <div className="container">
+        {colorScales.orangeRedScale.colors.map((obj, index) => {
+          return (
+            <div className="row" key={index}>
+              <div className="col">{obj.val}:</div>
+              {/*This approach below is not mobile friendly */}
+              <div className="col">
+                <div
+                  style={{
+                    height: "15px",
+                    width: "15px",
+                    backgroundColor: obj.color
+                  }}
+                ></div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+    ReactDOM.render(jsx, document.getElementById("colorLegend"));
+  }
 
-  console.log("render call");
-  if(props.pointsload){    
+  if (props.pointsload) {
     const jsx = <div {...props}>{props.children}</div>;
     L.Control.legend = L.Control.extend({
       onAdd: function(map) {
@@ -38,9 +61,10 @@ const LegendControl = function LegendControl(props) {
     if (props.map !== null) {
       L.control.legend({ position: "topright" }).addTo(props.map);
     }
-    return <div></div>;} else {
-      return <div></div>
-    }
+    return <div></div>;
+  } else {
+    return <div></div>;
+  }
 };
 
 class MapComponent extends React.Component {
@@ -48,66 +72,79 @@ class MapComponent extends React.Component {
     points: [],
     map: null,
     pointsLoad: false,
-    gridVizOn: false,
+    gridVizOn: false
   };
 
   async fetchNdjson() {
-    if(this.props.clustered){
-      const lat1=38.794, lng1=-77.14, lat2=38.997, lng2=-76.891;
-        //const lat1 = 38.884832, lng1 = -77.026726, lat2 = 38.882803, lng2 = -77.023405;
-        const url = 
-        "https://sidewalk-dc.cs.washington.edu/v2/access/attributes?lat1=" + lat1 + 
-                                                                   "&lng1=" + lng1 + 
-                                                                   "&lat2=" + lat2 + 
-                                                                   "&lng2=" + lng2;        
+    if (this.props.clustered) {
+      const lat1 = 38.794,
+        lng1 = -77.14,
+        lat2 = 38.997,
+        lng2 = -76.891;
+      //const lat1 = 38.884832, lng1 = -77.026726, lat2 = 38.882803, lng2 = -77.023405;
+      const url =
+        "https://sidewalk-dc.cs.washington.edu/v2/access/attributes?lat1=" +
+        lat1 +
+        "&lng1=" +
+        lng1 +
+        "&lat2=" +
+        lat2 +
+        "&lng2=" +
+        lng2;
       fetch(url)
-          .then(response => {
-              return response.json();
-          })
-          .then(data => {
-              if(this.props.excludecurbs) {
-                  //watch out: query returns property as label_type
-                  //excludes curbRamps
-                  data.features = data.features.filter(function(feature) 
-                    {return feature.properties["label_type"] !== "CurbRamp" && feature.properties["label_type"] !== "NoSidewalk"}
-                  );
-                  console.log(data);
-              }            
-              return data;
-          })
-            .then(data => {              
-              this.geojson.addData(data);
-              this.setState({points: data, pointsLoad: true});
-            });      
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          if (this.props.excludecurbs) {
+            //watch out: query returns property as label_type
+            //excludes curbRamps
+            data.features = data.features.filter(function(feature) {
+              return (
+                feature.properties["label_type"] !== "CurbRamp" &&
+                feature.properties["label_type"] !== "NoSidewalk"
+              );
+            });
+            console.log(data);
+          }
+          return data;
+        })
+        .then(data => {
+          this.geojson.addData(data);
+          this.setState({ points: data, pointsLoad: true });
+        });
     } else {
       //local data
       fetch("http://localhost:5555/api") // make a fetch request to a NDJSON stream service
-      .then(response => {
-        return ndjsonStream(response.body); //ndjsonStream parses the response.body
-      })
-      .then(exampleStream => {
-        const reader = exampleStream.getReader();
-        let read;
-        reader.read().then(
-          (read = result => {
-            if (result.done) {
-              //get geojson and set to state              
-              this.setState({ points: this.geojson.toGeoJSON(), pointsLoad: true });
-              return;
-            }
-            //load point by point onto map
-            const point = result.value; //returns a geojson object
-            console.log(point);
-            this.geojson.addData(point);
-            reader.read().then(read);
-          })
-        );
-      });
-    }    
+        .then(response => {
+          return ndjsonStream(response.body); //ndjsonStream parses the response.body
+        })
+        .then(exampleStream => {
+          const reader = exampleStream.getReader();
+          let read;
+          reader.read().then(
+            (read = result => {
+              if (result.done) {
+                //get geojson and set to state
+                this.setState({
+                  points: this.geojson.toGeoJSON(),
+                  pointsLoad: true
+                });
+                return;
+              }
+              //load point by point onto map
+              const point = result.value; //returns a geojson object
+              console.log(point);
+              this.geojson.addData(point);
+              reader.read().then(read);
+            })
+          );
+        });
+    }
   }
 
   componentWillMount() {}
-  
+
   componentDidMount() {
     // create map
     var southWest = L.latLng(38.794, -77.14),
@@ -148,14 +185,13 @@ class MapComponent extends React.Component {
           return "#6592d6";
         case "Occlusion":
           //find color for occlusion
-          return "#FF0000"
+          return "#FF0000";
         default:
           console.log(feature);
           return "#FFF";
       }
     }
 
-    this.pane1 = this.map.createPane('points');
     this.pointLayer = new L.FeatureGroup();
 
     this.geojson = L.geoJSON([], {
@@ -169,7 +205,7 @@ class MapComponent extends React.Component {
           fillOpacity: 0.8
         });
       }
-    })
+    });
 
     this.pointLayer.addLayer(this.geojson);
     this.map.addLayer(this.pointLayer);
@@ -182,41 +218,27 @@ class MapComponent extends React.Component {
     this.setState({ map: this.map });
   }
 
-  //generates the grid visualization
   gridMapViz = () => {
-    //parameters to change for grid aggregation
-    
-    //Johnson's style
-    /*
-    function getColor(d) {        
-      return d > 100 ? '#800026' :
-             d > 50  ? '#BD0026' :
-             d > 40  ? '#E31A1C' :
-             d > 30  ? '#FC4E2A' :
-             d > 20   ? '#FD8D3C' :
-             d > 10   ? '#FEB24C' :
-             d === 0  ? '#FFFFFF' :
-                        '#FFEDA0';      
-    }*/
-
+    //generates the grid visualization
     //modularized getColor
-    function getColor(d, colorScale) {        
-      const { colors } = colorScale;
+    const getColor = (d, colorScales) => {
+      const orangeRedScale = colorScales;
+      const { colors } = orangeRedScale;
+      console.log(colors);
       for (const color of colors) {
-        if(d > color.val) {
+        if (d > color.val) {
           return color.color;
         }
       }
+    };
 
-    }
-
-    if (this.state.map) {      
+    if (this.state.map) {
       //grid layer creation
       var sidewalkGridStyle = {
         style: function style(feature) {
           return {
-            weight: 0, //2 
-            fillColor: getColor(feature.properties.pointCount, orangeRedScale),
+            weight: 0, //2
+            fillColor: getColor(feature.properties.pointCount, colorScales),
             opacity: 0, //1
             color: "white",
             fillOpacity: feature.properties.pointCount > 0 ? 0.9 : 0
@@ -224,7 +246,7 @@ class MapComponent extends React.Component {
         }
       };
 
-      var dcLayer = L.geoJSON(dcBounds);      
+      var dcLayer = L.geoJSON(dcBounds);
 
       //GRID
       var bbox = dcLayer
@@ -235,17 +257,18 @@ class MapComponent extends React.Component {
       var cellSide = 0.25;
       var mask = dcBounds.features[0];
       var units = "kilometers";
-       var options = {
+      var options = {
         units: units,
         mask: mask
       };
 
       var grid = squareGrid(bbox, cellSide, options);
       console.log(grid);
-      console.log("finished generating grid, loading onto map");  
-      const len = grid.features.length;          
-      grid.features.forEach((feature, index) => {      
-        const pointCount = pointsWithinPolygon(this.state.points, feature).features.length;
+      console.log("finished generating grid, loading onto map");
+      const len = grid.features.length;
+      grid.features.forEach((feature, index) => {
+        const pointCount = pointsWithinPolygon(this.state.points, feature)
+          .features.length;
         console.log(feature, index, "/", len, "point count = ", pointCount);
         feature.properties["pointCount"] = pointCount;
       });
@@ -259,23 +282,30 @@ class MapComponent extends React.Component {
       button.disabled = true;*/
 
       //remove the points layer
-      
 
-      //this will probably break the code
-      this.setState({gridVizOn: true, pointsLoad: false});
+      //save the state
+      this.setState({ gridVizOn: true, pointsLoad: false });
     }
   };
 
-  handleTest = () => {
-    querySidwalkData();
-  }
-
-  render() {    
+  render() {
     return (
       <div id="map">
-        <LegendControl className="supportLegend" map={this.state.map} pointsload={this.state.pointsLoad}>
+        <LegendControl
+          className="supportLegend"
+          map={this.state.map}
+          pointsload={this.state.pointsLoad}
+          gridon={this.state.gridVizOn}
+        >
           <h1>This is a Test</h1>
-          <button id="gridViz" disabled={this.state.gridVizOn} onClick={this.gridMapViz}>Show Grid Viz</button>                    
+          <button
+            id="gridViz"
+            disabled={this.state.gridVizOn}
+            onClick={this.gridMapViz}
+          >
+            Show Grid Viz
+          </button>
+          <div id="colorLegend"></div>
         </LegendControl>
       </div>
     );
--- new-visualization-code/viz-client/src/utils/colorScales.js
+++ new-visualization-code/viz-client/src/utils/colorScales.js
@@ -1,4 +1,3 @@
-
 //to introduce a new scale, add it as a constant below in the defined format
 /*
 const <name of scale> = {
@@ -10,18 +9,47 @@ const <name of scale> = {
   ]
 }
 */
+
+//AccessViz scale
 const orangeRedScale = {
   colors: [
-    {val: 50, color: '#BD1527'},
-    {val: 30, color: '#F03B20'},
-    {val: 20, color: '#FE8D3A'},
-    {val: 15, color: '#FFB24B'},
-    {val: 10, color: '#FED975'},
-    {val: 5, color: '#FFFFB3'},             
-    {val: 0, color: '#FFFFFF'}
+    { val: 50, color: "#BD1527" },
+    { val: 30, color: "#F03B20" },
+    { val: 20, color: "#FE8D3A" },
+    { val: 15, color: "#FFB24B" },
+    { val: 10, color: "#FED975" },
+    { val: 5, color: "#FFFFB3" },
+    { val: 0, color: "#FFFFFF" }
   ],
   properties: []
 };
 
-export default orangeRedScale;
+//Curb Ramp scale
+const greenScale = {
+  colors: [
+    { val: 50, color: "#116837" },
+    { val: 30, color: "#31A353" },
+    { val: 20, color: "#77C679" },
+    { val: 15, color: "#ADDD8E" },
+    { val: 10, color: "#DAF0A3" },
+    { val: 5, color: "#FFFFCC" },
+    { val: 0, color: "#FFFFFF" }
+  ],
+  properties: []
+};
 
+//Custom-defined scale
+const customJohnsonScale = {
+  colors: [
+    { val: 50, color: "#800026" },
+    { val: 30, color: "#BD0026" },
+    { val: 20, color: "#E31A1C" },
+    { val: 15, color: "#FC4E2A" },
+    { val: 10, color: "#FD8D3C" },
+    { val: 5, color: "#FEB24C" },
+    { val: 0, color: "#FFFFFF" }
+  ],
+  properties: []
+};
+
+export default { orangeRedScale, greenScale, customJohnsonScale };
